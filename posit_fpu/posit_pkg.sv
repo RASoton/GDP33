0c318eedb7a09a6d34eaa24b10dd12d8fa269e6f
Sheng Yang Wong <129278646+ColinWong-529@users.noreply.github.com>
2023-10-17 07:57:33 +0100

    posit_classifier
    - create a new folder "posit_fpu"
    - implement posit classifier in systemverilog

---
+++ posit_fpu/posit_pkg.sv
@@ -0,0 +1,15 @@
+package posit_pkg;
+
+  // ---------
+  // POSIT TYPES
+  // ---------
+  // | Enumerator | Format           | Width  | REGIME | EXP_BITS | MAN_BITS
+  // |:----------:|------------------|-------:|:------:|:--------:|:--------:
+  // | POSIT32    | POSIT binary32   | 32 bit | r      | 2        | 29-r
+
+
+
+
+
+
+
