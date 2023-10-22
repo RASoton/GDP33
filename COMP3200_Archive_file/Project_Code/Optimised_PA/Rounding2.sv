1877c520f902a7102114ebb74026a7dc8159c295
BrianLChen <chenletian17011@hotmail.com>
2023-10-22 04:03:33 +0100

    change Rounding2.sv

--- COMP3200_Archive_file/Project_Code/Optimised_PA/Rounding2.sv
+++ COMP3200_Archive_file/Project_Code/Optimised_PA/Rounding2.sv
@@ -26,7 +26,8 @@ module Rounding #(parameter N = 32, parameter ES = 2, parameter RS = $clog2(N))
     input  logic LS,
     input  logic inf1, inf2,
     input  logic zero1, zero2,
-    output logic [N-1:0] OUT
+    output logic [N-1:0] OUT,
+    output logic [3:0] rounding_map
 );
 
 logic [(N+ES+N+3)-1:0] tmp_o;
@@ -38,12 +39,13 @@ logic [N:0] sft_tmp_o_rnd_ulp;
 logic [N-1:0] sft_tmp_o_rnd;
 logic [N-1:0] sft_tmp_oN;
 // Letian Chen 
-logic round, L, G, R, S, round_overflow;
+logic round, L, G, G2, R, S, round_overflow, round_condition;
 logic [2*N-1:0] regime_temp_output; // store regime and sign
 logic [N-1:0] regime_output;
-logic [N-1:0] exp_frac_output;
-logic [N+1:0] exp_frac_combine_output, rounding_temp, rounding_temp1;
+logic [N-1:0] exp_frac_output, exp_frac_output1, exp_frac_temp_output;
+logic [N+1:0] exp_frac_combine_output, rounding_temp, rounding_temp1, rounding_temp2;
 logic [N-1:0] temp_output, temp_output1;
+logic [1:0] overflow_shift;
 
 always_comb
 begin
@@ -51,13 +53,16 @@ begin
     exp_frac_combine_output = {1'b0,E_O[ES-1:0],Add_Mant_N[N-2:0]}; // combine 1-Overflow bit, 2-Exponent bit, 31-fraction bit
     // Do rounding Here
     // rounding_temp = exp_frac_combine_output << (N-R_O+1);
-    rounding_temp = exp_frac_combine_output << (N-R_O);
-    // rounding_temp1 = rounding_temp >> (N-R_O+1);
-    rounding_temp1 = rounding_temp;
+    rounding_temp = exp_frac_combine_output << (N-R_O-2);
+    rounding_temp1 = rounding_temp >> (N-R_O+1);
+    // rounding_temp1 = rounding_temp;
     L = rounding_temp[N+1];
     G = rounding_temp[N];
+    G2 = |rounding_temp[N:0];
     R = rounding_temp[N-1];
-    S = |rounding_temp[N-2:0];
+    S = |rounding_temp[N-1:0];
+    rounding_map = {L,G,R,S};
+    rounding_temp2 = exp_frac_combine_output >> R_O;
     
     // if(G==1'b0)
     // round = 1'b0;
@@ -66,29 +71,53 @@ begin
     // else
     // round = 1'b1;
     // round = (G&(R|S)) | (L&G&(~(R|S)));
+    // round = (~L&~G&~S)|(~L&~G&~R)|(~R&~S)|(L&G&R);
+    // ---------- Banker's Rounding
+    // if(G2 == 1'b1 && L == 1'b1)
+    // round = 1;
+    // else if(S == 1'b1 && L == 1'b0)
+    // round = 1;
+    // else if(R&S ==1'b1 && L == 1'b1)
+    // round = 1;
+    // else
+    // round = 0;
+    // ---------- round half to even
+    round_condition = G;
+    if(round_condition)
+    begin
+        if(S)
+        round = 1'b1;
+        else
+        round = L;
+    end
+    else
+    round = '0;
     // Finish Rounding
     
     // Pick usefull bit from rounded object
-    exp_frac_output = exp_frac_combine_output[N:2] >> (R_O+1); // Shift the Exponent bit and Fration bit to mach the regime and sign region
-    // exp_frac_output = exp_frac_output + round;
-    round_overflow = exp_frac_output[N-1];
+    exp_frac_temp_output = exp_frac_combine_output[N+1:2] + (round << (R_O+1));
+    round_overflow = exp_frac_temp_output[N-1];
+    exp_frac_output1 = exp_frac_combine_output[N+1:2] >> (R_O+1-round_overflow); // Shift the Exponent bit and Fration bit to mach the regime and sign region
+    exp_frac_output = exp_frac_output1 + round;
+    // round_overflow = exp_frac_temp_output[N-1];
+    // overflow_shift = {round_overflow, 0};
 
     // Handle Regime
     if(LE_O[ES+RS]) // When the exponents is 
-    regime_temp_output = 32'b1 << (2*N-R_O-2);
+    regime_temp_output = 1 << (2*N-R_O-2+round_overflow);
     else // When the exponents is 
-    regime_temp_output = ~(32'b1) << (2*N-R_O-2);
+    regime_temp_output = ~(1) << (2*N-R_O-2-round_overflow);
 
     regime_output = regime_temp_output[2*N-1:N];
     regime_output[N-1] = 1'b0; // Keep 1st bit of the output = 0 before handle sign
 
-    temp_output = regime_output | exp_frac_output; // conbine regime + exponent_fraction
+    temp_output1 = regime_output | exp_frac_output; // conbine regime + exponent_fraction
 
     // Change the Sign of the final result
     if(LS)
-    temp_output = -temp_output;
+    temp_output = -temp_output1;
     else
-    temp_output = temp_output;
+    temp_output = temp_output1;
 
 
     // //  N bits 0 or 1, following a terminating bit, exponent bits, (N-ES-1) bits mantissa, 3 bits for rounding
